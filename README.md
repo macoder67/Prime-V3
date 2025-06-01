c769da284a55ae0fe99891dd8e999d751db96036
macoder67 <manyakaontiretse@gmail.com>
2025-06-01 13:19:50 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Prime-V3
-The best WhatsApp bot in SA🇿🇦
+#### PRIME-V3 WHATSAPP BOT
+Izumi - Simple whatsapp Multi Device whatsapp bot.   
+### SETUP
+1. get session id
+    <br>
+<a href='https://connect.eypz.infy.uk/' target="_blank"><img alt='SESSION' src='https://img.shields.io/badge/SESSION-100000?style=for-the-badge&logo=scan&logoColor=white&labelColor=black&color=black'/></a>
+
+### DEPLOY TO RENDER 
+1. Now Deploy
+    <br>
+<a href='https://macoder-session.vercel.app/render' target="_blank"><img alt='DEPLOY' src='https://img.shields.io/badge/-DEPLOY-black?style=for-the-badge&logo=render&logoColor=white'/></a>
+
+
+### DEPLOY TO KOYEB
+1. Deploy on Koyeb
+   <br>
+<a href='https://macoder-session.vercel.app/koyeb' target="_blank"><img alt='Deploy' src='https://img.shields.io/badge/-Deploy-black?style=for-the-badge&logo=koyeb&logoColor=white'/></a>
+ 
+### RUN ON VPS/TERMUX
+
+1. Install packages
+   ```
+   apt update && apt upgrade -y && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+   ./start-ubuntu.sh
+   apt update && apt upgrade
+   apt install sudo
+   sudo apt install ffmpeg
+   sudo apt install imagemagick
+   sudo apt install yarn
+   sudo apt install git
+   sudo apt install curl
+   sudo apt -y remove nodejs
+   curl -fsSl https://deb.nodesource.com/setup_lts.x | sudo bash - && sudo apt -y install nodejs
+   ```
+2. installation
+   ```
+   git clone https://github.com/macoder67/Prime-V3
+   cd Prime-V3
+4. Configuration
+   ```
+   echo "SESSION_ID = prime~enulefekap
+   PORT = 8000
+   TERMUX = true">config.env
+
+   ```
+   edit your details
+- Start
+  ```
+  npm install
+  npm start
+  ```
+- Stop
+  ```
+  pm2 delete Prime-V3
+
+[![Tutorial](https://img.shields.io/badge/Tutorial-white?style=for-the-badge&logo=Prime-V3&logoColor=white)](https://t.me/27656136438)
+[![Join us on Telegram](https://img.shields.io/badge/Contact_Us_On_Telegram-blue?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/27656136438)
+
+<a href="https://whatsapp.com/channel/0029Vb5kA7k4IBhNW3Hhxy0s"><img alt="WhatsApp" src="https://img.shields.io/badge/-Whatsapp%20Channel-white?style=for-the-badge&logo=whatsapp&logoColor=black"/></a>
+
+
+## License
+
+This project is licensed under the [GNU General Public License v3.0](LICENSE).
---
+++ config.js
@@ -0,0 +1,55 @@
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+require('dotenv').config();
+
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env', override: true });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+const toBool = (x) => (x && x.toLowerCase() === 'true') || false;
+global.apiUrl = 'https://api.maskser.me/'
+global.eypzApi = 'https://api2.eypz.infy.uk/'
+global.Api = 'https://api.eypz.infy.uk/'
+
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL;
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+
+module.exports = {
+  HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
+  BRANCH: "main",
+  ADMIN_ACCESS: toBool(process.env.ADMIN_ACCESS) || false,
+  MODE: (process.env.MODE || 'private').toLowerCase(),
+  ERROR_MSG: toBool(process.env.ERROR_MSG) || true,
+  LOG_MSG: toBool(process.env.LOG_MSG) || true,
+  READ_CMD: toBool(process.env.READ_CMD),
+  ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE) || false,
+  WARN_COUNT: process.env.WARN_COUNT  || '3',
+  AUTO_STATUS_VIEW: toBool(process.env.AUTO_STATUS_VIEW) || false,
+  SESSION_ID: process.env.SESSION_ID || "Prime",
+  MENU_URL: process.env.MENU_URL || "https://cdn.eypz.ct.ws/url/15-05-25_06-18_w9fi.png",
+  CAPTION: process.env.CAPTION || "Made with 🤍",
+  READ_MSG: toBool(process.env.READ_MSG),
+  OWNER_NAME: process.env.OWNER_NAME || "Eʏᴘᴢ",
+  BOT_NAME: process.env.BOT_NAME || "Iᴢᴜᴍɪ-ᴍᴅ",
+  SUDO: process.env.SUDO || null,
+  LANG: process.env.LANGUAGE === undefined ? 'EN' : process.env.LANGUAGE.toUpperCase(),
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || "Iᴢᴜᴍɪ-ᴍᴅ,Eʏᴘᴢ",
+  AUDIO_DATA: process.env.AUDIO_DATA || "ᴍᴀᴄᴏᴅᴇʀ;ᴘʀɪᴍᴇ-ᴠ3;https://cdn.eypz.ct.ws/url/15-05-25_06-18_w9fi.png",
+  PROCESSNAME: process.env.PROCESSNAME || "Iᴢᴜᴍɪ-ᴍᴅ",
+  AUTHOR: process.env.AUTHOR || "ᴍᴀᴄᴏᴅᴇʀ",
+  PRESENCE: process.env.PRESENCE || "unavailable", //available, typing, recording
+  DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  KOYEB_API_KEY: process.env.KOYEB_API_KEY || "api",
+  KOYEB_APP_NAME: process.env.KOYEB_APP_NAME || "name",
+  KOYEB: toBool(process.env.KOYEB) || false,
+  HEROKU: toBool(process.env.HEROKU) || false,
+  TERMUX: toBool(process.env.TERMUX) || false,
+  DATABASE_URL: DATABASE_URL,
+  DATABASE:
+       DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
+  DEBUG: DEBUG
+};
